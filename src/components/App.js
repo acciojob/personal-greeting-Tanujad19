52aaae41666e535e539558615d53b888f3b96335
Tanujad19 <tanuja.work19@gmail.com>
2026-01-29 21:37:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [name, setName] = useState(""); 
+
   return (
     <div>
       <p>Enter your name:</p>
-     <input
-       type="text"
-       value={name}
-       onChange={(e) => setName(e.target.value)}
-     />
-     {name ? <p>Hello {name}!</p> : null}
-    </div>
-  )
-}
 
-export default App
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      {name && <p>Hello, {name}!</p>}
+    </div>
+  );
+};
+
+export default App;
